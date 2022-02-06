d57c85b2f7427369e6ff837de73532f71219f724
sylverainv <sylverainv@gmail.com>
2022-02-06 12:16:55 -0500

    add about me

--- vs-react-portfolio/src/App.js
+++ vs-react-portfolio/src/App.js
@@ -1,28 +1,13 @@
 import React from "react";
 import About from "./components/About";
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <main>
         <About></About>
       </main>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
     </div>
   );
 }
