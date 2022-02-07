5b99db1758e590b1104e712425fc5e498761dd6d
sylverainv <sylverainv@gmail.com>
2022-02-07 07:26:07 -0500

    add changes

--- vs-react-portfolio/src/components/About/index.js
+++ vs-react-portfolio/src/components/About/index.js
@@ -1,30 +1,35 @@
 import React from "react";
 import coverImage from "/Users/vanessasylverain/Desktop/My-React-Portfolio/VS-React-Portfolio/vs-react-portfolio/src/assets/cover/coverimage.jpeg";
+import headShot from "//Users/vanessasylverain/Desktop/My-React-Portfolio/VS-React-Portfolio/vs-react-portfolio/src/assets/cover/headshot.jpeg";
 
-function About() {
-  return (
-    <section className="my-5">
-      <h1 id="about">About Me</h1>
-      <img
-        src={coverImage}
-        className="my-2"
-        style={{ width: "100%" }}
-        alt="cover"
-      />
-      <div className="my-2">
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
-          bibendum turpis sed ex condimentum molestie. Mauris condimentum lectus
-          ut ornare dignissim. Mauris faucibus urna mi, ac feugiat metus aliquam
-          maximus. Proin aliquam justo nec diam vulputate vestibulum. Aenean
-          sollicitudin nulla at nisi ornare, nec suscipit massa eleifend. Morbi
-          tristique justo vel turpis sollicitudin, et tristique velit convallis.
-          In hac habitasse platea dictumst. Phasellus mattis nunc sed orci
-          consequat laoreet. Praesent id nisl nibh. Curabitur imperdiet
-          ultricies mollis. In hac habitasse platea dictumst.
-        </p>
-      </div>
-    </section>
+const About = () => {
+    return (
+        <><div className="container-fluid">
+        <div className="a" id="about">
+          <div className="row">
+            <div className="col-md-6">
+              <div className="a-left">
+                <div className="a-card bg"></div>
+                <div className="a-card">
+                  <img src={headShot} alt="" className="a-img" />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div><section className="my-5">
+          <h1 id="about">About Me</h1>
+          <img
+            src={coverImage}
+            className="my-2"
+            style={{ width: "100%" }}
+            alt="cover" />
+          <div className="my-2">
+            <p>
+              In 2012, I embarked on my journey to become a Nursing Home Administrator but later realizing that I have an affinity for technology and asthetics. I decided to take a leap of faith and working towards a career that allows me to utilize my leadership strengths as well as my tactical skills.
+            </p>
+          </div>
+        </section></>
   );
 }
 
