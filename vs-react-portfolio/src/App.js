a634720364e977234b042567e43a9b199137c464
sylverainv <sylverainv@gmail.com>
2022-02-06 20:20:51 -0500

    update about

--- vs-react-portfolio/src/App.js
+++ vs-react-portfolio/src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import About from "./components/About";
 import "./App.css";
+import coverImage from "./vs-react-portfolio/src/assets/cover/digetal flower.jpeg";
 
 function App() {
   return (
